9565eb040afcf794d37eb5afbf0314f0e012b49c
Sophia Davis <scdgrapefruit@gmail.com>
2014-09-24 19:03:13 -0400

    Implement resizing

--- hash_table.c
+++ hash_table.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 /***
 *
@@ -29,19 +30,20 @@ typedef struct node {
 typedef struct hash_table {
     int size;
     int load;
+    float max_load_proportion;
     Node **items_list;
 } HashTable;
 
 /***
 * Function declarations
 ***/
-HashTable* init(int size);
+HashTable *init(int size, float max_load_proportion);
 void print_table(HashTable *hash_table);
 void print_item(Item *item);
 
 int calculate_hash(union Hashable key);
 
-void add_item_to_table(union Hashable key, union Hashable value, HashTable *hash_table);
+HashTable *add_item_to_table(int hash, union Hashable key, union Hashable value, HashTable *hash_table);
 Node *add_item_to_bin(Item *item, Node *items_list);
 
 Item *lookup(union Hashable key, HashTable *hash_table);
@@ -49,12 +51,14 @@ Item *lookup(union Hashable key, HashTable *hash_table);
 Item *remove_item_from_table(union Hashable key, HashTable *hash_table);
 Node *remove_item_from_bin(union Hashable key, Node *items_list);
 
+HashTable *resize(HashTable *hash_table);
+
 
 /***
 * Program
 ***/
 int main() {
-    HashTable *hash_table = init(4);
+    HashTable *hash_table = init(4, 0.5);
     print_table(hash_table);
 
     union Hashable key;
@@ -66,31 +70,43 @@ int main() {
     key.i = 0;
     value.i = 0;
     printf("\n~~~~~Adding %i -- %i (hash: %i)\n", key.i, value.i, calculate_hash(key));
-    add_item_to_table(key, value, hash_table);
+    hash_table = add_item_to_table(HUGE_VAL, key, value, hash_table);
     print_table(hash_table);
 
-    // key.i = 1;
-    // value.i = 1;
-    // printf("\n~~~~~Adding %i -- %i (hash: %i)\n", key.i, value.i, calculate_hash(key));
-    // add_item_to_table(key, value, hash_table);
-    // print_table(hash_table);
+    key.i = 1;
+    value.i = 1;
+    printf("\n~~~~~Adding %i -- %i (hash: %i)\n", key.i, value.i, calculate_hash(key));
+    hash_table = add_item_to_table(HUGE_VAL, key, value, hash_table);
+    print_table(hash_table);
 
     key.i = 2;
     value.i = 2;
     printf("\n~~~~~Adding %i -- %i (hash: %i)\n", key.i, value.i, calculate_hash(key));
-    add_item_to_table(key, value, hash_table);
+    hash_table = add_item_to_table(HUGE_VAL, key, value, hash_table);
     print_table(hash_table);
 
     key.i = 3;
     value.i = 3;
     printf("\n~~~~~Adding %i -- %i (hash: %i)\n", key.i, value.i, calculate_hash(key));
-    add_item_to_table(key, value, hash_table);
+    hash_table = add_item_to_table(HUGE_VAL, key, value, hash_table);
     print_table(hash_table);
 
     key.i = 7;
     value.i = 4;
     printf("\n~~~~~Adding %i -- %i (hash: %i)\n", key.i, value.i, calculate_hash(key));
-    add_item_to_table(key, value, hash_table);
+    hash_table = add_item_to_table(HUGE_VAL, key, value, hash_table);
+    print_table(hash_table);
+
+    key.i = 277;
+    value.i = 4;
+    printf("\n~~~~~Adding %i -- %i (hash: %i)\n", key.i, value.i, calculate_hash(key));
+    hash_table = add_item_to_table(HUGE_VAL, key, value, hash_table);
+    print_table(hash_table);
+
+    key.i = 277;
+    value.i = 5;
+    printf("\n~~~~~Adding %i -- %i (hash: %i)\n", key.i, value.i, calculate_hash(key));
+    hash_table = add_item_to_table(HUGE_VAL, key, value, hash_table);
     print_table(hash_table);
 
     printf("~~~~~~~~~~~~~~~ Testing Lookup ~~~~~~~~~~~~~~~\n");
@@ -120,18 +136,36 @@ int main() {
 
     print_table(hash_table);
 
+    key.i = 2;
+    removed = remove_item_from_table(key, hash_table);
+    print_item(removed);
+
+    key.i = 1;
+    removed = remove_item_from_table(key, hash_table);
+    print_item(removed);
+
+    key.i = 0;
+    removed = remove_item_from_table(key, hash_table);
+    print_item(removed);
+
+    print_table(hash_table);
+
+    key.i = 0;
+    removed = remove_item_from_table(key, hash_table);
+    print_item(removed);
+
     return 0;
 }
 
-HashTable* init(int size) {
+HashTable *init(int size, float max_load_proportion) {
     HashTable *hash_table = malloc(sizeof(HashTable));
     hash_table->size = size;
+    hash_table->max_load_proportion = max_load_proportion;
     hash_table->load = 0;
+    hash_table->items_list = malloc(size*sizeof(Node*));
 
     int i;
     for (i = 0; i < size; i++) {
-        printf("Creating bin %i.\n", i);
-
         Node *null_node;
         null_node = NULL;
 
@@ -144,8 +178,13 @@ int calculate_hash(union Hashable key) {
     return key.i; // TODO actually hash the keys
 }
 
-void add_item_to_table(union Hashable key, union Hashable value, HashTable *hash_table) {
-    int hash = calculate_hash(key);
+HashTable *add_item_to_table(int hash, union Hashable key, union Hashable value, HashTable *hash_table) {
+    if (((float)(hash_table->load + 1) / (float)hash_table->size) > hash_table->max_load_proportion){
+        hash_table = resize(hash_table);
+    }
+    if (hash == (int)HUGE_VAL) {
+        hash = calculate_hash(key);
+    }
 
     Item *item = malloc(sizeof(Item));
     item->hash = hash;
@@ -158,6 +197,7 @@ void add_item_to_table(union Hashable key, union Hashable value, HashTable *hash
     hash_table->items_list[bin_index] = add_item_to_bin(item, items_list);
 
     hash_table->load++;
+    return hash_table;
 }
 
 Node *add_item_to_bin(Item *item, Node *items_list) {
@@ -165,7 +205,6 @@ Node *add_item_to_bin(Item *item, Node *items_list) {
     Node *prev_node;
     Node *current_node = items_list;
     if (current_node == NULL) {
-        printf("Adding to empty bin\n");
         Node *new = malloc(sizeof(Node));
         new->item = item;
         new->next = current_node;
@@ -175,13 +214,12 @@ Node *add_item_to_bin(Item *item, Node *items_list) {
         while (current_node != NULL) {
             Item *current_item = current_node->item;
             if (current_item->key.i == item->key.i) {
-                printf("Replacing value for key %i\n", item->key.i);
+                current_item->value.i = item->value.i;
                 return head;
             }
             prev_node = current_node;
             current_node = current_node->next;
         }
-        printf("Adding to end of bin\n");
         Node *new = malloc(sizeof(Node));
         new->item = item;
         new->next = current_node;
@@ -257,12 +295,27 @@ Node *remove_item_from_bin(union Hashable key, Node *items_list) {
         return head;
     }
 }
-// hash
 
-// resize
+HashTable *resize(HashTable *old_hash_table) {
+    printf("Resizing from %i to %i.\n", old_hash_table->size, 2*old_hash_table->size);
+    HashTable *new_hash_table = init(2*old_hash_table->size, old_hash_table->max_load_proportion);
+    new_hash_table->load = 0;
+
+    int i;
+    for (i = 0; i < old_hash_table->size; i++) {
+        Node *current_node = old_hash_table->items_list[i];
+        while (current_node != NULL) {
+            Item *to_copy = current_node->item;
+            new_hash_table = add_item_to_table(to_copy->hash, to_copy->key, to_copy->value, new_hash_table);
+            current_node = current_node->next;
+        }
+    }
+    return new_hash_table;
+}
+
 
 void print_table(HashTable *hash_table) {
-    printf("\n********************\n--------HashTable--------\n-Array size: %i\n-Load: %i\n", hash_table->size, hash_table->load);
+    printf("\n********************\n--------HashTable--------\n-Array size: %i -Load: %i -Max Load Prop: %f -Current Load Prop: %f\n", hash_table->size, hash_table->load, hash_table->max_load_proportion, ((float)hash_table->load / (float)hash_table->size));
 
     int i;
     for (i = 0; i < hash_table->size; i++) {
