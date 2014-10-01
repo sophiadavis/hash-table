3d2f5fdc16e5c0985fef4ae4d12074fef4cb65c9
Sophia Davis <scdgrapefruit@gmail.com>
2014-10-01 17:02:18 -0400

    Format code

--- hashtablemodule.c
+++ hashtablemodule.c
@@ -204,51 +204,46 @@ static PyMethodDef HashTablePy_methods[] = {
 
 static PyTypeObject HashTablePyType = {
     PyObject_HEAD_INIT(NULL)
-    0,                         /*ob_size*/
-    "hashtable.HashTable",             /*tp_name*/
-    sizeof(HashTablePyObject), /*tp_basicsize*/
-    0,                         /*tp_itemsize*/
-    (destructor)HashTablePyObject_dealloc, /*tp_dealloc*/
-    (printfunc)HashTablePy_print,                         /*tp_print*/
-    0,                         /*tp_getattr*/
-    0,                         /*tp_setattr*/
-    0,                         /*tp_compare*/
-    0,                         /*tp_repr*/
-    0,                         /*tp_as_number*/
-    0,                         /*tp_as_sequence*/
-    0,                         /*tp_as_mapping*/
-    0,                         /*tp_hash */
-    0,                         /*tp_call*/
-    0,                         /*tp_str*/
-    0,                         /*tp_getattro*/
-    0,                         /*tp_setattro*/
-    0,                         /*tp_as_buffer*/
-    Py_TPFLAGS_DEFAULT,        /*tp_flags*/
-    "Customizeable HashTable!",       /* tp_doc */
-    0,		               /* tp_traverse */
-    0,		               /* tp_clear */
-    0,		               /* tp_richcompare */
-    0,		               /* tp_weaklistoffset */
-    0,		               /* tp_iter */
-    0,		               /* tp_iternext */
-    HashTablePy_methods,    /* tp_methods */
-    Hashtable_members,             /* tp_members */
-    0,                         /* tp_getset */
-    0,                         /* tp_base */
-    0,                         /* tp_dict */
-    0,                         /* tp_descr_get */
-    0,                         /* tp_descr_set */
-    0,                         /* tp_dictoffset */
-    (initproc)HashTablePyObject_init,      /* tp_init */
-    0,                          /* tp_alloc */
-    0,                          /* tp_new */
-    (freefunc)HashTablePyObject_free, /* tp_free */
+    0,                                           /* ob_size */
+    "hashtable.HashTable",                       /* tp_name */
+    sizeof(HashTablePyObject),                   /* tp_basicsize */
+    0,                                           /* tp_itemsize */
+    (destructor)HashTablePyObject_dealloc,       /* tp_dealloc */
+    (printfunc)HashTablePy_print,                /* tp_print */
+    0,                                           /* tp_getattr */
+    0,                                           /* tp_setattr */
+    0,                                           /* tp_compare */
+    0,                                           /* tp_repr */
+    0,                                           /* tp_as_number */
+    0,                                           /* tp_as_sequence */
+    0,                                           /* tp_as_mapping */
+    0,                                           /* tp_hash */
+    0,                                           /* tp_call */
+    0,                                           /* tp_str */
+    0,                                           /* tp_getattro */
+    0,                                           /* tp_setattro */
+    0,                                           /* tp_as_buffer */
+    Py_TPFLAGS_DEFAULT,                          /* tp_flags */
+    "Customizeable HashTable!",                  /* tp_doc */
+    0,		                                     /* tp_traverse */
+    0,		                                     /* tp_clear */
+    0,		                                     /* tp_richcompare */
+    0,		                                     /* tp_weaklistoffset */
+    0,		                                     /* tp_iter */
+    0,		                                     /* tp_iternext */
+    HashTablePy_methods,                         /* tp_methods */
+    Hashtable_members,                           /* tp_members */
+    0,                                           /* tp_getset */
+    0,                                           /* tp_base */
+    0,                                           /* tp_dict */
+    0,                                           /* tp_descr_get */
+    0,                                           /* tp_descr_set */
+    0,                                           /* tp_dictoffset */
+    (initproc)HashTablePyObject_init,            /* tp_init */
+    0,                                           /* tp_alloc */
+    0,                                           /* tp_new */
+    (freefunc)HashTablePyObject_free,            /* tp_free */
 };
-
-// static PyMethodDef module_methods[] = {
-//     {NULL}  /* Sentinel */
-// };
-
 
 #ifndef PyMODINIT_FUNC	/* declarations for DLL import/export */
 #define PyMODINIT_FUNC void
